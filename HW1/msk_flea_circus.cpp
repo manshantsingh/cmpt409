aad42c20a05e2d6695b489806b62f2f4defbb235
Manshant Singh Kohli <me@manshantsingh.com>
2018-01-17 20:36:44 -0800

    finished all 4 problems

---
+++ HW1/msk_flea_circus.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    while(true){
+        int n; cin>>n;
+        if(n==0) break;
+
+        vector<vector<int>> v(n, vector<int>());
+        while(--n){
+            int a, b; cin>>a>>b;
+            a--;b--;
+            v[a].push_back(b);
+            v[b].push_back(a);
+        }
+        int l; cin>>l;
+        while(l--){
+            int a, b; cin>>a>>b;
+            if(a==b){
+                cout<<"The fleas meet at "<<a<<"."<<endl;
+                continue;
+            }
+            a--;b--;
+            queue<pair<int,int>> q;
+            q.push({a,0});
+            int d;
+            vector<int> p(v.size(),-1);
+            while(true){
+                int c; tie(c,d) = q.front();
+                d++;
+                q.pop();
+                bool flag=false;
+                for(int x: v[c]){
+                    if(p[x]!=-1) continue;
+                    p[x]=c;
+                    if(x==b){
+                        flag=true;
+                        break;
+                    }
+                    q.push({x,d});
+                }
+                if(flag) break;
+            }
+            bool odd=d%2;
+            d/=2;
+            int x=b;
+            while(d--) x=p[x];
+            if(odd){
+                set<int> s{x+1, p[x]+1};
+                cout<<"The fleas jump forever between "<<*s.begin()<<" and "<<*s.rbegin()<<"."<<endl;
+            }
+            else cout<<"The fleas meet at "<<x+1<<"."<<endl;
+        }
+    }
+}
