0f407ba60af919214234c86397193123e0ec7d83
Manshant Singh Kohli <me@manshantsingh.com>
2018-02-09 14:03:33 -0800

    Adding HW3 - Unidirectional TSP

---
+++ HW3/msk_116_Unidirectional_TSP.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int m,n;
+    while(cin>>m>>n){
+        vector<vector<int>> grid(m, vector<int>(n)), dp(m, vector<int>(n));
+        for(auto& x: grid) for(auto& y: x) cin>>y;
+
+        for(int i=n-2;i>=0;i--){ //column
+            for(int j=0;j<m;j++){ //row
+                set<int> s{j, (j+1)%m, (j+m-1)%m};
+                auto it=s.begin();
+                int val=grid[*it][i+1];
+                int index=*it;
+                while(++it != s.end()){
+                    int newVal = grid[*it][i+1];
+                    if(newVal < val){
+                        val=newVal;
+                        index=*it;
+                    }
+                }
+                dp[j][i]=index;
+                grid[j][i]+=val;
+            }
+        }
+
+        int mm=0;
+        for(int i=1;i<m;i++){ //row
+            if(grid[i][0]<grid[mm][0]) mm=i;
+        }
+        cout<<mm+1;
+        for(int i=0, x=mm;i<n-1;i++){ //row
+            x=dp[x][i];
+            cout<<' '<<x+1;
+        }
+        cout<<endl<<grid[mm][0]<<endl;
+    }
+}
