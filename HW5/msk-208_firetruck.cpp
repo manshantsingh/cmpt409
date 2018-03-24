b29271b860e118532651460e56430ca9e527fd81
Manshant Singh Kohli <me@manshantsingh.com>
2018-03-23 19:10:04 -0700

    HW5 - accepted firetruck

--- HW5/msk-208_firetruck.cpp
+++ HW5/msk-208_firetruck.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int LIMIT = 21;
+
 int n, total;
-unordered_map<int, vector<int>> m;
-unordered_map<int,int> parent;
+int parent[LIMIT];
+vector<int> m[LIMIT];
+int reachable;
 
 void func(int a){
     for(int x: m[a]){
@@ -22,10 +25,33 @@ void func(int a){
             }
             cout<<' '<<n<<endl;
         }
-        else if(parent.find(x)==parent.end()){
+        // prune if a path doesn't exist or if the node is already visited
+        else if((reachable & (1<<x))!=0 && parent[x]==-1){
             parent[x]=a;
             func(x);
-            parent.erase(x);
+            parent[x]=-1;
+        }
+    }
+}
+
+// runs BFS from the endpoint to see if there see if a path exists from point x to n
+void computeReachable(int n){
+    vector<int> mm[LIMIT];
+    for(auto& x: mm) x.clear();
+    for(int i=1;i<LIMIT;i++){
+        for(int x: m[i]) mm[x].push_back(i);
+    }
+    reachable=(1<<n);
+    queue<int> q;
+    q.push(n);
+    while(q.size()){
+        int a = q.front();
+        q.pop();
+        for(int x: mm[a]){
+            if((reachable & (1<<x))==0){
+                reachable |= 1<<x;
+                q.push(x);
+            }
         }
     }
 }
@@ -34,8 +60,7 @@ int main(){
     ios_base::sync_with_stdio(false);
     int caseNum=0;
     while(cin>>n){
-        m.clear();
-        parent.clear();
+        for(auto& x: m) x.clear();
         while(true){
             int a,b;
             cin>>a>>b;
@@ -50,8 +75,10 @@ int main(){
             total++;
         }
         else {
-            for(auto& x: m) sort(x.second.begin(), x.second.end());
+            for(auto& x: m) sort(x.begin(), x.end());
+            for(int i=2;i<LIMIT;i++) parent[i]=-1;
             parent[1]=1;
+            computeReachable(n);
             func(1);
         }
         cout<<"There are "<<total<<" routes from the firestation to streetcorner "<<n<<"."<<endl;
